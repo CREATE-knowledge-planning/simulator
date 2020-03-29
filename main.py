31f161263d622059d0680989fc320f6dd8d86213
Antoni Viros Martin <ani300@gmail.com>
2020-03-29 15:42:41 -0500

    Added code to generate a single example mission

---
+++ main.py
@@ -0,0 +1,49 @@
+from mission_creation.create_logic import create_logic
+from mission_creation.kg_additions import add_mission
+
+
+def write_mln_evidence(evidence, output_path):
+    names_dict = {}
+    with open(output_path + '.db', 'w') as evidence_file:
+        for evidence_piece in evidence:
+            uids = []
+            for element in evidence_piece['elements']:
+                uids.append('{}{}'.format(
+                    list(element.labels)[0],
+                    element.id
+                ))
+                names_dict[uids[-1]] = element['name']
+            evidence_str = "{}({})\n".format(
+                evidence_piece['type'],
+                ",".join(uids)
+            )
+            evidence_file.write(evidence_str)
+    with open(output_path + '.dict', 'w') as dict_file:
+        for uid, name in names_dict.items():
+            dict_file.write('{}: {}\n'.format(uid, name))
+
+
+def write_mln_program(predicates, formulas_path, output_path):
+    with open(output_path + '.mln', 'w') as program_file:
+        program_file.write('// predicate declarations\n')
+        for predicate in predicates:
+            predicate_str = '{}({})\n'.format(
+                predicate['type'],
+                ','.join(predicate['node_types'])
+            )
+            program_file.write(predicate_str)
+        program_file.write('\n// formulas\n')
+        with open(formulas_path) as formulas_file:
+            program_file.write(formulas_file.read())
+
+
+def main():
+    # Generate mission and make a file of it in logic format
+    add_mission()
+    evidence, predicates = create_logic()
+    write_mln_evidence(evidence, 'output')
+    write_mln_program(predicates, 'formulas.mln', 'output')
+
+
+if __name__ == "__main__":
+    main()
---
+++ mission_creation/create_logic.py
@@ -0,0 +1,84 @@
+from random import random
+
+from neo4j import GraphDatabase
+
+
+def save_evidence(session, results, evidence, predicates, predicate_types):
+    for record in results:
+        for relationship in record:
+            start_node = session.run('MATCH (n) WHERE id(n) = {id} RETURN n',
+                                     id=relationship.start_node.id).single().value()
+            end_node = session.run('MATCH (n) WHERE id(n) = {id} RETURN n',
+                                   id=relationship.end_node.id).single().value()
+            if relationship.type not in predicate_types:
+                predicate_types.add(relationship.type)
+                predicates.append({
+                    'type': relationship.type,
+                    'node_types': [
+                        list(start_node.labels)[0],
+                        list(end_node.labels)[0]
+                    ]
+                })
+            evidence.append({
+                'type': relationship.type,
+                'elements': [
+                    start_node,
+                    end_node
+                ]
+            })
+
+
+def create_logic():
+    uri = "bolt://localhost:7687"
+    driver = GraphDatabase.driver(uri, auth=("neo4j", "test"))
+
+    evidence = []
+    predicates = []
+    name_matching = {}
+    predicate_types = set()
+
+    with driver.session() as session:
+        # Generate evidence and predicates for sensors and platforms
+        results = session.run('MATCH (s:Sensor)-[r:HOSTS]-(p:Platform) '
+                              'WHERE p.status = "Currently being flown" '
+                              'RETURN r')
+        save_evidence(session, results, evidence, predicates, predicate_types)
+
+        # Generate evidence and predicates for sensors and observable properties
+        results = session.run('MATCH (p:Platform)-[:HOSTS]-(s:Sensor)-[r:OBSERVES]-(op:ObservableProperty) '
+                              'WHERE p.status = "Currently being flown" '
+                              'RETURN r')
+        save_evidence(session, results, evidence, predicates, predicate_types)
+
+        # Generate evidence and predicates for mission and observations
+        results = session.run('MATCH (m:Mission)-[r:REQUIRES]-(ob:Observation) '
+                              'WHERE m.mid = 1 '
+                              'RETURN r')
+        save_evidence(session, results, evidence, predicates, predicate_types)
+
+        # Generate evidence and predicates for observations and its observable properties
+        results = session.run('MATCH (m:Mission)-[:REQUIRES]-(ob:Observation)-[r:OBSERVEDPROPERTY]-(op:ObservableProperty) '
+                              'WHERE m.mid = 1 '
+                              'RETURN r')
+        save_evidence(session, results, evidence, predicates, predicate_types)
+
+        # Generate evidence and predicates for platforms being in visibility of the target
+        visibility_threshold = 0.8
+        predicate_types.add('inVisibilityOfTarget')
+        predicates.append({
+            'type': 'inVisibilityOfTarget',
+            'node_types': ['Platform']
+        })
+        results = session.run('MATCH (p:Platform) '
+                              'WHERE p.status = "Currently being flown" '
+                              'RETURN p')
+        for record in results:
+            platform_node = record.value()
+            randnum = random()
+            if randnum < visibility_threshold:
+                evidence.append({
+                    'type': 'inVisibilityOfTarget',
+                    'elements': [platform_node]
+                })
+
+        return evidence, predicates
